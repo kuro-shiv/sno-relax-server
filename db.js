afd825300caf8ffcb85543ae20c9fd8e12e0eda8
ankit <dubeysk2003@gmail.com>
2025-09-23 14:37:21 +0530

    con1

--- db.js
+++ db.js
@@ -4,9 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "snoRelaxDB",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      dbName: "snoRelaxDB", // your DB name
     });
 
     mongoose.connection.on("connected", () => {
