bb8ddc3ad754695111c4bed56629b82c116c6eb8
kuro-shiv <dubeysk2003@gmail.com>
2025-11-12 18:29:26 +0530

    auth

--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
-const mongoose = require('mongoose');
+// sno-relax-server/models/User.js
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  passwordHash: { type: String },
-  role: { type: String, default: 'user' },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
+    city: { type: String, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    avatar: { type: String, default: "https://i.imgur.com/KR0NKdM.png" },
+    history: { type: String, default: "" },
+    role: { type: String, default: "user" },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
